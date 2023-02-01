e330e9a69d6ffc60576fdfbd8f3f9ad181426208
mynameisjisoo <devjisoolee@gmail.com>
2023-02-01 10:30:06 +0900

    S3버킷 파일 업로드위해 export 명령어 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  exportPathMap: async function () {
+    const paths = {
+      "/": { page: "/" },
+      "/test": { page: "/test" },
+    };
+
+    return paths;
+  },
 };
 
 module.exports = nextConfig;
