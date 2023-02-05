e459278a91e39a76cafd0f7332586a35b1196cb2
강선우 <74632731+NiNinanana@users.noreply.github.com>
2023-02-05 11:54:09 +0900

    netlify 설정 적용 (#25)
    * netlfiy 설정 적용
    
    * build 명령어 수정
    
    * netlify.toml 에 마지막 줄 추가
    
    * 사용하지 않는 build 부분 제거
    
    * 플러그인을 devDependency로 옮김
    
    * next config 사용하지 않는 부분 삭제

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  exportPathMap: async function () {
-    const paths = {
-      "/": { page: "/" },
-      "/test": { page: "/test" },
-    };
-
-    return paths;
-  },
 };
 
 module.exports = nextConfig;
