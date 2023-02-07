3115e54fe0cb277bad92d2d4071922a0aa3869f6
Jisoo Lee <84840032+mynameisjisoo@users.noreply.github.com>
2023-02-07 19:20:50 +0900

    썸네일컴포넌트 구현 (#43)
    * 썸네일컴포넌트 구현, 태그 컴포넌트 구현, emotion props type 지정
    
    * 사용하지 않는 파일 삭제,
    더미데이터 컴포넌트 밖에서 주입
    사용하지 않는 key props 제거
    global box sizing 지정

---
+++ src/customer/components/StoreThumbnail.tsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import { css } from "@emotion/react";
+
+import { Colors, Texts } from "styles/common";
+import Tag from "customer/components/common/Tag";
+import Pick from "public/icons/Pick";
+
+interface ThumbnailProps {
+  content: {
+    id: string;
+    store: string;
+    category: string;
+    tags: string[];
+    location: string;
+    img: string;
+  };
+}
+
+const StoreThumbnail = ({ content }: ThumbnailProps) => {
+  const [isPick, setIsPick] = useState(false);
+
+  const IMG_SIZE = 152;
+
+  const thumbnailStyle = css`
+    cursor: pointer;
+    width: fit-content;
+  `;
+
+  const imgWrapper = css`
+    position: relative;
+    margin-bottom: 6px;
+    width: ${IMG_SIZE}px;
+    height: ${IMG_SIZE}px;
+  `;
+
+  const imgStyle = css`
+    border-radius: 4px;
+  `;
+
+  const btnStyle = css`
+    position: absolute;
+    right: 4px;
+    bottom: 4px;
+    background-color: transparent;
+    cursor: pointer;
+  `;
+
+  const storeStyle = css`
+    ${Texts.B3_15_R2}
+    margin-right: 4px;
+  `;
+
+  const categoryStyle = css`
+    ${Texts.C2_12_R}
+    color: ${Colors.neutral60};
+  `;
+
+  const locationStyle = css`
+    ${Texts.C2_12_R}
+    color: ${Colors.neutral70};
+  `;
+
+  const tagStyle = css`
+    display: flex;
+    gap: 4px;
+  `;
+
+  const onPickClick = () => {
+    setIsPick((prev) => !prev);
+    // Todo : 찜한 가게 저장
+  };
+  return (
+    <>
+      <div css={thumbnailStyle}>
+        <div css={imgWrapper}>
+          <Image
+            src={content.img}
+            alt={content.store}
+            width={IMG_SIZE}
+            height={IMG_SIZE}
+            css={imgStyle}
+          />
+          <button css={btnStyle} onClick={onPickClick}>
+            <Pick isPick={isPick} />
+          </button>
+        </div>
+        <div>
+          <span css={storeStyle}>{content.store}</span>
+          <span css={categoryStyle}>{content.category}</span>
+        </div>
+        <div css={tagStyle}>
+          {content.tags.map((tag) => (
+            <Tag text={tag} key={tag} />
+          ))}
+        </div>
+        <span css={locationStyle}>{content.location}</span>
+      </div>
+    </>
+  );
+};
+
+export default StoreThumbnail;
