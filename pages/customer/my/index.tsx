6b83fbe4fe6625053bd10b2fa9d054cf8dff8ff5
Jisoo Lee <84840032+mynameisjisoo@users.noreply.github.com>
2023-02-09 11:39:10 +0900

    Dialog 컴포넌트 구현 (#47)
    * Dialog 컴포넌트 구현
    
    * onClick 함수 정의
    
    * import 문 정렬
    버튼 classname 구체적으로 변경
    Dialog props에 name 속성 추가
    불필요한 공백 삭제
    
    * 코드리뷰 반영
    버튼  프로퍼티  변경
    주석 하이라이트 처리
    공백 제거
    additional text 렌더링

--- pages/customer/my/index.tsx
+++ pages/customer/my/index.tsx
@@ -1,12 +1,55 @@
 import React from "react";
+import { css } from "@emotion/react";
 
+import { Colors, Texts } from "styles/common";
 import Layout from "customer/components/common/layout";
+import Dialog from "customer/components/mypage/Dialog";
 
+const highlight = css`
+  color: ${Colors.amber50};
+  ${Texts.B3_15_B}
+`;
+
+const addText = css`
+  ${Texts.B3_15_R2}
+`;
+
+// FIXME: Review용 (Modal 완성되면 삭제정예정)
+const test = css`
+  background-color: grey;
+  padding: 2rem;
+`;
+
+const dialogContent = [
+  {
+    usage: "withdrawal" as const,
+    id: "aaa1234",
+    name: "물고기1234",
+    additionalText: (
+      <div css={addText}>
+        단골손님에서 <span css={highlight}>더 많은 혜택</span>들이 기다리고 있어요!
+      </div>
+    ),
+    buttonText: { confirm: "탈퇴하기", cancel: "혜택 계속 사용하기" },
+  },
+  {
+    usage: "unsubscribe" as const,
+    id: "aaa1234",
+    name: "물고기1234",
+    buttonText: { confirm: "구독 해지하기", cancel: "구독 유지하기" },
+  },
+];
 const My = () => {
+  const onConfirm = () => {};
+  const onCancel = () => {};
   return (
     <>
       <Layout title="마이페이지" subTitle="마이페이지">
-        <div>My</div>
+        <div css={test}>
+          <Dialog content={dialogContent[0]} onConfirm={onConfirm} onCancel={onCancel} />
+          <br />
+          <Dialog content={dialogContent[1]} onConfirm={onConfirm} onCancel={onCancel} />
+        </div>
       </Layout>
     </>
   );
---
+++ src/customer/components/mypage/Dialog.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { css } from "@emotion/react";
+
+import { Colors, Texts } from "styles/common";
+
+type DialogProps = {
+  content: {
+    usage: "withdrawal" | "unsubscribe";
+    id: string;
+    name: string;
+    additionalText?: JSX.Element;
+    buttonText: { confirm: string; cancel: string };
+  };
+  onConfirm: () => void;
+  onCancel: () => void;
+};
+
+const wrapper = css`
+  background-color: ${Colors.white};
+  color: ${Colors.black};
+  width: 20rem;
+  padding: 2rem 1.25rem;
+  border-radius: 0.625rem;
+  text-align: center;
+`;
+
+const headingWrapper = css`
+  margin-bottom: 1.5rem;
+`;
+
+const heading = css`
+  ${Texts.S3_18_B}
+  white-space: pre;
+`;
+
+const buttonWrapper = css`
+  display: flex;
+  justify-content: center;
+  gap: 0.75rem;
+
+  & button {
+    width: 100%;
+    border-radius: 4px;
+    padding: 0.563rem 0;
+    color: ${Colors.white};
+    ${Texts.B3_15_M2}
+  }
+
+  & button.confirmBtn {
+    background-color: ${Colors.neutral50};
+  }
+  & button.cancelBtn {
+    background-color: ${Colors.amber50};
+  }
+`;
+
+const Dialog = ({ content, onConfirm, onCancel }: DialogProps) => {
+  return (
+    <div css={wrapper}>
+      <div css={headingWrapper}>
+        <h1 css={heading}>
+          {content.usage === "withdrawal"
+            ? `${content.name}님,\n정말 회원 탈퇴하시겠습니까?`
+            : `${content.name},\n구독 해지하겠습니까?`}
+        </h1>
+        {content.additionalText}
+      </div>
+      <div css={buttonWrapper}>
+        <button className="confirmBtn" onClick={onConfirm}>
+          {content.buttonText.confirm}
+        </button>
+        <button className="cancelBtn" onClick={onCancel}>
+          {content.buttonText.cancel}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Dialog;
