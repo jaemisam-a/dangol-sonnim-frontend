e6e59c432128902511f6bb7a67b93afe66f84414
Jisoo Lee <84840032+mynameisjisoo@users.noreply.github.com>
2023-02-09 10:11:58 +0900

    상단 검색바 구현 (#46)
    * 상단 검색바 구현
    
    * custom.d.ts 삭제, type import 해서 사용
    
    * Todo highlight

---
+++ src/customer/components/SearchBar.tsx
@@ -0,0 +1,63 @@
+import React, { useRef, KeyboardEvent } from "react";
+import { css } from "@emotion/react";
+
+import { Colors } from "styles/common";
+import Search from "public/icons/Search.svg";
+
+const inputWrapper = css`
+  position: relative;
+  width: fit-content;
+`;
+
+const input = css`
+  background-color: ${Colors.neutral20};
+  border-radius: 4px;
+  width: 20rem;
+  height: 2.5rem;
+  padding: 0.5rem 3.25rem 0.5rem 0.75rem;
+  border: 0;
+  caret-color: ${Colors.amber50};
+
+  &:focus {
+    outline: solid 2px ${Colors.amber50};
+  }
+
+  &::-webkit-search-cancel-button {
+    -webkit-appearance: none;
+    width: 20px;
+    height: 20px;
+    background: url("icons/Close.svg") center center no-repeat;
+    cursor: pointer;
+  }
+`;
+
+const searchIcon = css`
+  position: absolute;
+  right: 0.75rem;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
+const SearchBar = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      // TODO: 검색
+    }
+  };
+
+  return (
+    <div css={inputWrapper}>
+      <input
+        type="search"
+        placeholder="음식 이름, 구독권 이름 검색"
+        ref={inputRef}
+        css={input}
+        onKeyDown={onKeyDown}
+      />
+      <Search css={searchIcon} />
+    </div>
+  );
+};
+
+export default SearchBar;
