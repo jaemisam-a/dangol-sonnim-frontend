196bbc777f4518fee247ae2e0c3a911febd0d7e7
강선우 <74632731+NiNinanana@users.noreply.github.com>
2023-02-13 15:16:09 +0900

    마이페이지 탭 컴포넌트 구현 (#51)
    * 마이페이지 탭 컴포넌트 구현
    
    * import문 정렬
    
    * key 값 수정

---
+++ src/customer/components/mypage/Tab.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { css } from "@emotion/react";
+
+import { Colors, Texts } from "styles/common";
+
+const wrapper = css`
+  display: flex;
+`;
+
+const tabBtn = (tab: boolean) => css`
+  width: 50%;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${tab ? Colors.amber50 : Colors.neutral40};
+  border-bottom: ${tab ? `3px solid ${Colors.amber50}` : `1px solid ${Colors.neutral40}`};
+  cursor: pointer;
+  ${Texts.B3_15_B}
+`;
+
+const Tab = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
+  const tabArr = ["내가 구독한 쿠폰", "좋아요 가게"];
+
+  return (
+    <>
+      <div css={wrapper}>
+        {tabArr.map((tab, idx) => (
+          <div key={tab} css={tabBtn(selectedTab === idx)} onClick={() => setSelectedTab(idx)}>
+            {tab}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Tab;
