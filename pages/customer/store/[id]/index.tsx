d4747226f15661c729633ab2e58a58b45194035d
NiNinanana <yeen00123@gmail.com>
2023-02-14 10:18:16 +0900

    가게 상세페이지 구독권 구현

--- pages/customer/store/[id]/index.tsx
+++ pages/customer/store/[id]/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 
 import Layout from "customer/components/common/layout";
+import Subs from "customer/components/store/Subs";
 
 const Store = () => {
+  const storeName = "정갈한솥";
+
   return (
     <>
       <Layout title="가게이름">
         <div>Store</div>
+        <Subs storeName={storeName} />
       </Layout>
     </>
   );
