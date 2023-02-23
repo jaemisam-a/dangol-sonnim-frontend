44863fe79193c490682e4d6aff3e437708620864
NiNinanana <yeen00123@gmail.com>
2023-02-23 16:00:40 +0900

    home page에 push 추가

--- next.config.js
+++ next.config.js
@@ -12,15 +12,15 @@ const nextConfig = {
     });
     return config;
   },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/customer",
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/customer",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
