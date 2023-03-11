eb62f67e668ff153bf0e316ebf85f166a36eb598
NiNinanana <yeen00123@gmail.com>
2023-03-11 15:11:27 +0900

    #136 가게 위치 등록 페이지 구현

---
+++ pages/owner/settings/location.tsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+import { useInfiniteQuery } from "react-query";
+import { useInView } from "react-intersection-observer";
+import axios from "axios";
+import { css } from "@emotion/react";
+
+import Layout from "common/layout";
+import SearchBar from "common/input/Search";
+import LocationList, { checkedAddrType } from "common/LocationList";
+
+const wrapper = css`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1.5rem 1.25rem;
+`;
+
+const SettingsLocation = () => {
+  const [ref, inView] = useInView();
+  const [query, setQuery] = useState("");
+  const [checkedAddr, setCheckedAddr] = useState<checkedAddrType>({ roadAddr: "", idx: null });
+
+  const { data, refetch, fetchNextPage } = useInfiniteQuery(
+    "location",
+    ({ pageParam = 1 }) =>
+      axios
+        .post(
+          "/juso/addrlink/addrLinkApiJsonp.do",
+          {
+            confmKey: process.env.NEXT_PUBLIC_JUSO_VALIDATION_KEY,
+            keyword: query,
+            resultType: "json",
+            countPerPage: 20,
+            currentPage: pageParam,
+          },
+          { headers: { "content-type": "application/x-www-form-urlencoded" } }
+        )
+        .then((res) => res),
+    {
+      getNextPageParam: (lastPage: any) =>
+        Number(JSON.parse(lastPage.data.slice(1, -1)).results.common.currentPage) + 1,
+    }
+  );
+
+  const locationListData = data && JSON.parse(data?.pages.at(-1)?.data.slice(1, -1)).results;
+
+  useEffect(() => {
+    if (!data) return;
+    const { countPerpage, totalCount, currentpage } = locationListData;
+    /** 계속 fetch되는 것을 막기 위해 현재 페이지가 마지막 페이지라면 return */
+    if (
+      (totalCount % countPerpage ? totalCount / countPerpage + 1 : totalCount / countPerpage) ===
+      currentpage
+    )
+      return;
+    /** 마지막 ref가 보여지면 다음 페이지 fetch */
+    if (inView) fetchNextPage();
+  }, [inView]);
+
+  return (
+    <Layout title="가게 위치 등록" subTitle="위치 등록" isCheckButton={true}>
+      <div css={wrapper}>
+        <SearchBar
+          isCustomer={false}
+          placeholder="언주로 170"
+          setState={setQuery}
+          mutate={refetch}
+        />
+        <div>
+          {locationListData &&
+            data?.pages.map((res: any) => {
+              const locationData = JSON.parse(res.data.slice(1, -1)).results.juso;
+              if (!locationData) return;
+              return locationData.map((el: any, idx: number) => (
+                <LocationList
+                  key={el.roadAddr}
+                  idx={idx}
+                  jibunAddr={el.jibunAddr}
+                  roadAddr={el.roadAddr}
+                  checkedAddr={checkedAddr}
+                  setCheckedAddr={setCheckedAddr}
+                  lastRef={ref}
+                  dataLength={locationData.length}
+                />
+              ));
+            })}
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default SettingsLocation;
---
+++ src/components/common/LocationList.tsx
@@ -0,0 +1,84 @@
+import React, { Dispatch, SetStateAction } from "react";
+import { css } from "@emotion/react";
+
+import { Colors, Texts } from "styles/common";
+import CheckedRadio from "public/icons/check/CheckedRadio.svg";
+import UnCheckedRadio from "public/icons/check/UnCheckedRadio.svg";
+
+export type checkedAddrType = { roadAddr: string; idx: number | null };
+
+type LocationListProps = {
+  idx: number;
+  roadAddr: string;
+  jibunAddr: string;
+  checkedAddr: checkedAddrType;
+  setCheckedAddr: Dispatch<SetStateAction<checkedAddrType>>;
+  lastRef: (node?: Element | null | undefined) => void;
+  dataLength: number;
+};
+
+const wrapper = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.75rem 0;
+  border-bottom: 1px solid #d9d9d9;
+`;
+
+const addrWrapper = css`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+  ${Texts.B2_14_R2}
+`;
+
+const roadArr = css`
+  color: #1a1a1a;
+`;
+
+const jibunWrapper = css`
+  display: flex;
+  gap: 0.438rem;
+`;
+
+const jibunTag = css`
+  display: flex;
+  align-items: center;
+  padding: 0px 0.375rem;
+  border-radius: 7.5rem;
+  background-color: ${Colors.amber50};
+  color: white;
+  ${Texts.C1_11_B}
+`;
+
+const jibunAddr = css`
+  color: #464646;
+`;
+
+const radioButton = css`
+  height: fit-content;
+  background-color: transparent;
+`;
+
+const LocationList = (props: LocationListProps) => {
+  const handleRadio = () => {
+    props.setCheckedAddr({ roadAddr: props.roadAddr, idx: props.idx });
+  };
+
+  return (
+    <div css={wrapper} ref={props.idx === props.dataLength - 1 ? props.lastRef : null}>
+      <div css={addrWrapper}>
+        <div css={roadArr}>{props.roadAddr}</div>
+        <div css={jibunWrapper}>
+          <span css={jibunTag}>지번</span>
+          <span css={jibunAddr}>{props.jibunAddr}</span>
+        </div>
+      </div>
+      <button css={radioButton} onClick={handleRadio}>
+        {props.idx === props.checkedAddr.idx ? <CheckedRadio /> : <UnCheckedRadio />}
+      </button>
+    </div>
+  );
+};
+
+export default LocationList;
