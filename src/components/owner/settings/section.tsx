a9da9b75d999096e4cdeefef0821b6d0b60c6fac
강선우 <74632731+NiNinanana@users.noreply.github.com>
2023-03-18 00:30:43 +0900

    #158 popoverUI 구현 및 추가 (#170)
    * #158 popoverUI 구현 및 추가
    
    * status를 옵셔널로 수정
    
    * popover 배경 색 추가 및 type 수정

--- src/components/owner/settings/section.tsx
+++ src/components/owner/settings/section.tsx
@@ -1,8 +1,10 @@
-import React, { ReactNode } from "react";
+import React, { Dispatch, ReactNode, SetStateAction, useRef, useState } from "react";
+import { usePopper } from "react-popper";
 import { css } from "@emotion/react";
 
 import Kebab from "public/icons/menu/kebab.svg";
 import { Colors, Texts } from "styles/common";
+import Popover, { statusType } from "common/popover";
 
 type StoreSectionProps = {
   children: ReactNode;
@@ -10,6 +12,8 @@ type StoreSectionProps = {
   menuCount?: number;
   isLocation: boolean;
   isEmpty?: boolean;
+  status?: statusType;
+  setStatus?: Dispatch<SetStateAction<statusType>>;
 };
 
 const wrapper = css`
@@ -43,6 +47,14 @@ const registrationButton = css`
 `;
 
 const StoreSection = (props: StoreSectionProps) => {
+  const [openPopover, setOpenPopover] = useState(false);
+  const kebabRef = useRef(null);
+  const popoverRef = useRef(null);
+
+  const { styles, attributes } = usePopper(kebabRef.current, popoverRef.current, {
+    placement: "bottom-end",
+  });
+
   return (
     <>
       <div css={wrapper}>
@@ -51,14 +63,28 @@ const StoreSection = (props: StoreSectionProps) => {
             <div css={title}>{props.sectionTitle}</div>
             <div css={count}>{props.menuCount}</div>
           </div>
-          {!props.isLocation &&
-            (props.isEmpty ? (
-              <button css={registrationButton}>{props.sectionTitle} 등록</button>
-            ) : (
-              <div>
-                <Kebab />
-              </div>
-            ))}
+          {!props.isLocation && (
+            <div>
+              {props.status === "default" ? (
+                <button css={registrationButton}>{props.sectionTitle} 등록</button>
+              ) : (
+                <>
+                  <button ref={kebabRef} onClick={() => setOpenPopover((prev) => !prev)}>
+                    <Kebab />
+                  </button>
+                  <Popover
+                    label={props.sectionTitle}
+                    isOpen={openPopover}
+                    setIsOpen={setOpenPopover}
+                    popoverRef={popoverRef}
+                    style={styles.popper}
+                    attributes={attributes.popper}
+                    setStatus={props.setStatus}
+                  />
+                </>
+              )}
+            </div>
+          )}
         </div>
         {props.children}
       </div>
