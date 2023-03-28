dd09f86f2655b639e1662f883f563c1684b6948d
NiNinanana <yeen00123@gmail.com>
2023-03-28 12:10:08 +0900

    코드리뷰 반영 ToastUI TODO 주석 작성 contents를 string배열로 수정 isTitleBold 대신 isContentsBold로 수정

--- src/components/owner/list/section.tsx
+++ src/components/owner/list/section.tsx
@@ -5,8 +5,8 @@ import { Colors, Texts } from "styles/common";
 
 type SubscriberSectionProps = {
   title: string;
-  contents: string;
-  isTitleBold: boolean;
+  contents: string[];
+  isContentsBold: boolean;
 };
 
 const wrapper = css`
@@ -22,17 +22,23 @@ const title = css`
   ${Texts.B3_15_M2}
 `;
 
-const contents = (isTitleBold: boolean) => css`
-  color: ${isTitleBold ? Colors.amber50 : Colors.black};
+const contents = (isContentsBold: boolean) => css`
+  display: flex;
+  flex-direction: column;
+  color: ${isContentsBold ? Colors.amber50 : Colors.black};
   white-space: pre-wrap;
-  ${isTitleBold ? Texts.S1_16_B : Texts.B3_15_M1}
+  ${isContentsBold ? Texts.S1_16_B : Texts.B3_15_M1}
 `;
 
 const SubscriberSection = (props: SubscriberSectionProps) => {
   return (
     <div css={wrapper}>
       <h2 css={title}>{props.title}</h2>
-      <div css={contents(props.isTitleBold)}>{props.contents}</div>
+      <div css={contents(props.isContentsBold)}>
+        {props.contents.map((el) => (
+          <span key={el}>{el}</span>
+        ))}
+      </div>
     </div>
   );
 };
