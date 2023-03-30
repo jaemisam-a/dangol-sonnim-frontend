d8f2b80043d37244ff7986533e377ead57af855b
NiNinanana <yeen00123@gmail.com>
2023-03-30 10:49:01 +0900

    next config proxy 수정

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
       },
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/:path*`,
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_URL}/api/:path*`,
       },
     ];
   },
