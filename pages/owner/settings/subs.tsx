881530b0d0b9d6d47db89d0755066d514096e9e9
NiNinanana <yeen00123@gmail.com>
2023-04-10 19:50:28 +0900

    구독권 등록 api 구현

--- pages/owner/settings/subs.tsx
+++ pages/owner/settings/subs.tsx
@@ -1,21 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from "react-query";
 import { useRouter } from "next/router";
 
 import Layout from "common/layout";
 import SubsForm from "owner/settings/subsForm";
+import { addSubsCoupon } from "pages/api/owner/subs";
+
+export type subsType = {
+  name: string;
+  type: "COUNT" | "MONTHLY";
+  count: number;
+  price: number;
+  benefit: string;
+  isMain: boolean;
+};
 
 const Subs = () => {
   const { query } = useRouter();
+
+  const { mutateAsync } = useMutation(addSubsCoupon);
+
+  const [subsContent, setSubsContent] = useState<subsType>({
+    name: "",
+    type: "MONTHLY",
+    count: 0,
+    price: 0,
+    benefit: "",
+    isMain: true,
+  });
   const isEdit = Boolean(query?.subsId);
+
+  const addSubs = () => {
+    mutateAsync({
+      name: subsContent.name,
+      type: subsContent.type,
+      intro: subsContent.benefit,
+      isTop: subsContent.isMain,
+      price: subsContent.price,
+      useCount: subsContent.count,
+      // FIXME: 가게 id를 query로 받아 적용
+      storeId: 1,
+    });
+  };
 
   return (
     <Layout
       title="메뉴 관리"
       subTitle={isEdit ? "구독권 수정" : "구독권 등록"}
       isXButton={true}
-      checkBtnFnc={() => {}}
+      checkBtnFnc={addSubs}
     >
-      <SubsForm isEdit={isEdit} />
+      <SubsForm isEdit={isEdit} subsContent={subsContent} setSubsContent={setSubsContent} />
     </Layout>
   );
 };
