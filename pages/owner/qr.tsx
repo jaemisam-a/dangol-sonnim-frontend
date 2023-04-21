8f3895f6f652f018f8e67e1cf9ac98f26a4fdc59
Jisoo Lee <84840032+mynameisjisoo@users.noreply.github.com>
2023-04-21 13:27:47 +0900

    #175 QR 코드 스캔 UI (#209)
    * #175 QR 코드 스캔
    
    * 토스트 알림 추가

--- pages/owner/qr.tsx
+++ pages/owner/qr.tsx
@@ -1,11 +1,38 @@
 import React from "react";
+import { css } from "@emotion/react";
+import { QrReader } from "react-qr-reader";
 
 import Layout from "common/layout";
+import ViewFinder from "public/icons/etc/viewfinder.svg";
+import useToastStore from "src/store/toast";
+
+const viewfinder = css`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+`;
 
 const QR = () => {
+  const { setMessage } = useToastStore();
+
   return (
     <Layout title="QR코드" isLogo={true}>
-      <div>QR</div>
+      <QrReader
+        ViewFinder={() => <ViewFinder css={viewfinder} />}
+        scanDelay={2000}
+        constraints={{ facingMode: "environment" }}
+        onResult={(result, error) => {
+          if (!!result) {
+            /** TODO: 방문 확인 API */
+            setMessage("방문이 확인되었습니다.", false, "info");
+          } else if (!!error) {
+            setMessage("QR코드 인증에 실패했습니다.\n다시 한번 확인해주세요.", false, "warning");
+          }
+        }}
+        videoStyle={{ width: "100%", heigth: "100vh" }}
+      />
     </Layout>
   );
 };
