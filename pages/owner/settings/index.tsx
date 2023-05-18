5d15f3788f809f42f3c782118df7df282d9d82a4
Jisoo Lee <84840032+mynameisjisoo@users.noreply.github.com>
2023-05-18 19:32:42 +0900

    #267 가게 설정페이지 API 연결(가게 조회) (#268)

--- pages/owner/settings/index.tsx
+++ pages/owner/settings/index.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { css } from "@emotion/react";
 import { useRouter } from "next/router";
 import { useQuery } from "react-query";
 import { useStore } from "zustand";
 
 import Layout from "common/layout";
-import Loading from "common/loading";
+import FullPageSpinner from "common/spinner/fullPage";
 import Picture from "owner/settings/picture";
 import Location from "owner/settings/location";
 import Menus from "owner/settings/menus";
 import Subs from "owner/settings/subs";
 import Info from "owner/settings/info";
-import { CreateStoreResDataType, getMyStoreList } from "pages/api/owner/dangolStore";
+import { findStore, getMyStoreList } from "pages/api/owner/dangolStore";
 import { categoryIdToString } from "src/utils/category";
 import useCurrentStore from "src/store/currentStore";
 import SettingButton from "owner/settings/settingButton";
@@ -23,47 +23,25 @@ const bottomPadding = css`
 `;
 
 const Settings = () => {
-  const menuDummyData = [
-    { id: 1, name: "불고기비빔밥", img: "/images/dummy/cheetah.jpg", price: 9500 },
-    { id: 2, name: "불고기비빔밥", img: "/images/dummy/cheetah.jpg", price: 9500 },
-    { id: 3, name: "불고기비빔밥", img: "/images/dummy/cheetah.jpg", price: 9500 },
-    { id: 4, name: "불고기비빔밥", img: "/images/dummy/cheetah.jpg", price: 9500 },
-    { id: 5, name: "불고기비빔밥", img: "/images/dummy/cheetah.jpg", price: 9500 },
-  ];
-
-  const subsDummyData = [
-    {
-      id: 1,
-      name: "모든 메뉴 사이즈업",
-      count: 5,
-      description: "쿠폰 소지시 최대 5회까지 전 메뉴 사이즈업 가능. 방문 1번당 최대 2번",
-      price: 3500,
-      isMain: true,
-      tags: ["사이드디쉬추가", "사이즈업"],
-      storeName: "정갈한솥",
-    },
-
-    {
-      id: 2,
-      name: "계란 추가 or 새우튀김 제공",
-      count: 5,
-      description: "쿠폰 소지시 최대 5회까지 전 메뉴 사이즈업 가능. 방문 1번당 최대 2번",
-      price: 3500,
-      isMain: false,
-      tags: ["사이드디쉬추가", "사이즈업"],
-      storeName: "정갈한솥",
-    },
-  ];
-
   const { push } = useRouter();
-  const { data, isLoading } = useQuery("getMyStoreList", getMyStoreList, {
-    // FIXME: 로그인 여부 확인하여 enable 상태 변경하도록 수정하기
-    enabled: true,
-  });
-  const { currentStoreId, setCurrentStoreId } = useStore(useCurrentStore);
   const { resetStoreInfo } = useStore(useMyStoreInfo);
+  const { currentStoreId, setCurrentStoreId } = useStore(useCurrentStore);
 
-  const [storeData, setStoreData] = useState<CreateStoreResDataType>();
+  const { data: mystoreData, isLoading: isMyStoreLoading } = useQuery(
+    "getMyStore",
+    getMyStoreList,
+    {
+      // FIXME: 로그인 여부 확인하여 enable 상태 변경하도록 수정하기
+      enabled: true,
+    }
+  );
+  const {
+    data: findStoreData,
+    isLoading: isFindStoreLoading,
+    refetch,
+  } = useQuery("findStoreInfo", () => findStore(currentStoreId), {
+    enabled: currentStoreId !== "",
+  });
 
   useEffect(() => {
     //FIXME: 로그인 안된 상태라면 로그인 페이지로 이동
@@ -73,56 +51,51 @@ const Settings = () => {
   }, []);
 
   useEffect(() => {
-    if (data?.length > 0 && currentStoreId === "") {
-      /** 등록한 가게가 있으면 기본적으로 가게 목록 중 첫번째가 보이도록 함 */
-      setStoreData(data[0]);
-      setCurrentStoreId(data[0].id);
-    } else if (data?.length < 1) {
+    if (mystoreData?.length > 0 && currentStoreId === "") {
+      /** 등록한 가게가 있으면 내 가게 목록 중 첫번째 스토어를 currentStore로 설정 */
+      setCurrentStoreId(mystoreData[0].id);
+    } else if (mystoreData?.length < 1) {
       /** 등록한 가게가 없는 경우에는 가게 정보 등록 페이지로 이동 */
       push("/owner/mystore");
     }
     /** mystore를 중간에 이탈 할 경우 전역변수를 reset해야함. mystore 페이지가 unmount 될 때 초기화하면 다음 페이지까지 입력한 값을 유지할 수 없으므로 가게 설정페이지에 들어오면 reset한다. */
     resetStoreInfo();
-  }, [data]);
+  }, [mystoreData]);
 
   useEffect(() => {
-    /** 사이드 네비바에서 설정한 가게 변경할 경우 */
-    if (data) {
-      const currentStore = data.find((el: CreateStoreResDataType) => el.id === currentStoreId);
-      if (currentStore) {
-        setStoreData(currentStore);
-      }
-    }
-  }, [currentStoreId, data]);
+    if (currentStoreId === "") return;
+    refetch();
+  }, [currentStoreId]);
 
-  /** 등록한 가게가 없어서 storeData가 빈 값인 경우에도 /mystore로 이동하는 동안에는 로딩을 렌더링한다. */
-  if (isLoading || !storeData) return <Loading />;
-
-  return (
-    <Layout title="가게설정" subTitle="가게 설정" isLogo={true}>
-      <Picture />
-      <Info
-        name={storeData?.name}
-        category={categoryIdToString(storeData?.categoryType)}
-        comments={storeData?.comments}
-      />
-      <Location
-        address={storeData?.newAddress}
-        detail={storeData?.detailedAddress}
-        openHour={storeData?.businessHours}
-      />
-      <SettingButton
-        heading="가게 정보 설정"
-        icon={<PencilUnderline />}
-        action={() =>
-          push({ pathname: "/owner/mystore", query: { isEdit: true } }, "/owner/mystore")
-        }
-      />
-      <Menus data={menuDummyData} />
-      <Subs data={subsDummyData} />
-      <div css={bottomPadding} />
-    </Layout>
-  );
+  if (isMyStoreLoading || isFindStoreLoading || !findStoreData || mystoreData?.length < 1) {
+    return <FullPageSpinner />;
+  } else {
+    return (
+      <Layout title="가게설정" subTitle="가게 설정" isLogo={true}>
+        <Picture images={findStoreData.storeImageUrlList} />
+        <Info
+          name={findStoreData.name}
+          category={categoryIdToString(findStoreData.categoryType)}
+          comments={findStoreData.comments}
+        />
+        <Location
+          address={findStoreData.newAddress}
+          detail={findStoreData.detailedAddress}
+          openHour={findStoreData.businessHours}
+        />
+        <SettingButton
+          heading="가게 정보 설정"
+          icon={<PencilUnderline />}
+          action={() =>
+            push({ pathname: "/owner/mystore", query: { isEdit: true } }, "/owner/mystore")
+          }
+        />
+        <Menus data={findStoreData.menuResponseDTOList} />
+        <Subs data={findStoreData.subscribeResponseDTOList} />
+        <div css={bottomPadding} />
+      </Layout>
+    );
+  }
 };
 
 export default Settings;
