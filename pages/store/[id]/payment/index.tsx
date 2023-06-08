a1d72afe08f51f46b9604e6778112fd81a7c5bef
강선우 <74632731+NiNinanana@users.noreply.github.com>
2023-06-08 23:29:45 +0900

    아임포트 적용하여 결제 테스트 (#288)
    * 아임포트 적용하여 결제 테스트
    
    * 코드리뷰 반영
    결제 시 주소 삭제
    카드 입력받는 Select 삭제
    
    * 주문번호 생성 후 적용

--- pages/store/[id]/payment/index.tsx
+++ pages/store/[id]/payment/index.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { css } from "@emotion/react";
 import { useRouter } from "next/router";
+import { useQuery } from "react-query";
 
 import Layout from "common/layout";
 import StoreCoupon from "common/coupon/store";
 import Consent from "common/consent";
+import Spinner from "common/spinner";
 import PaymentInfo from "customer/store/menu/payment/info";
 import PaymentMethod from "customer/store/menu/payment/method";
 import { Colors, Texts } from "styles/common";
+import { getSubs } from "pages/api/subs";
+import { getOrderNumber } from "src/utils/getOrderNumber";
 
 export type SelectedType = {
   id: string;
@@ -20,16 +24,7 @@ export type TransferType = {
   accountHolder: string;
   accountNumber: string;
 };
-
-const DUMMY_PAYMENT = {
-  id: 1,
-  storeName: "정갈한솥",
-  count: 5,
-  name: "모든 메뉴 사이즈업",
-  description: "쿠폰 소지시 최대 5회까지 전 메뉴 사이즈업 가능. 방문 1번당 최대 2번.",
-  price: 3500,
-  type: "COUNT",
-};
+declare const window: typeof globalThis & { IMP: any };
 
 const couponWrapper = css`
   padding: 0.75rem 1.25rem 0 1.25rem;
@@ -57,13 +52,20 @@ const buyButton = (isActive: boolean) => css`
 const StorePayment = () => {
   const { push, asPath, query, isReady } = useRouter();
 
+  const { data } = useQuery(
+    "subs",
+    () => getSubs({ subscribeId: Number(JSON.parse(query.selectedSubs as string)[0]) }),
+    {
+      enabled: Boolean(query.selectedSubs),
+    }
+  );
+
   const [selectMethod, setSelectMethod] = useState(1);
   const [cashReceipts, setCashReceipts] = useState<CashReceiptsType>({
     isPersonal: true,
     data: "",
     isUse: false,
   });
-  const [selectedCard, setSelectedCard] = useState({ id: "", name: "" });
   const [selectedBank, setSelectedBank] = useState<TransferType>({
     id: "",
     name: "",
@@ -72,8 +74,41 @@ const StorePayment = () => {
   });
   const [isConsent, setIsConsent] = useState(false);
 
+  const handlePayment = () => {
+    const paymentMethod =
+      selectMethod === 1
+        ? process.env.NEXT_PUBLIC_PG_KCP
+        : selectMethod === 3
+        ? process.env.NEXT_PUBLIC_PG_KAKOPAY
+        : selectMethod == 4 && process.env.NEXT_PUBLIC_PG_TOSSPAY;
+    window.IMP.request_pay(
+      {
+        pg: paymentMethod,
+        pay_method: "card",
+        merchant_uid: getOrderNumber(),
+        name: data.name,
+        amount: data.price,
+        // TODO: 계정정보 적용하여 입력
+        buyer_email: "gildong@gmail.com",
+        buyer_name: "홍길동",
+        buyer_tel: "010-4242-4242",
+      },
+      (rsp: any) => {
+        // TODO: 결제 정보를 저장할 수 있는 페이지가 나오면 추가
+        rsp.success
+          ? push(
+              {
+                pathname: `${asPath}/complete`,
+                query: { price: data.price },
+              },
+              `${asPath}/complete`
+            )
+          : alert("결제에 실패하였습니다. 다시 시도해주세요.");
+      }
+    );
+  };
+
   useEffect(() => {
-    setSelectedCard({ id: "", name: "" });
     setSelectedBank({ id: "", name: "", accountHolder: "", accountNumber: "" });
     setCashReceipts({ data: "", isPersonal: true, isUse: false });
   }, [selectMethod]);
@@ -82,7 +117,7 @@ const StorePayment = () => {
     const completeCashReceipts = cashReceipts.isUse ? cashReceipts.data : true;
     switch (selectMethod) {
       case 1:
-        return Boolean(selectedCard.id) && isConsent;
+        return isConsent;
       case 2:
         return (
           Boolean(selectedBank.id) &&
@@ -97,69 +132,81 @@ const StorePayment = () => {
   };
 
   useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://cdn.iamport.kr/v1/iamport.js";
+    script.async = true;
+
+    const handleScriptLoad = () => {
+      window.IMP?.init(process.env.NEXT_PUBLIC_PG_IMP_CODE);
+    };
+
+    script.addEventListener("load", handleScriptLoad);
+
+    document.head.appendChild(script);
+  }, []);
+
+  useEffect(() => {
     if (!isReady) return;
     if (!query.selectedSubs) push(`/store/${query.id}`);
   }, [isReady]);
 
   return (
     <Layout title="결제" subTitle="결제화면">
-      <div css={couponWrapper}>
-        {/* FIXME: 선택한 구독권 id에 맞는 api 요청 */}
-        {query.selectedSubs &&
-          (JSON.parse(query.selectedSubs as string) as string[]).map((subs) => (
-            <StoreCoupon
-              type={DUMMY_PAYMENT.type as "MONTHLY" | "COUNT"}
-              id={DUMMY_PAYMENT.id}
-              count={DUMMY_PAYMENT.count}
-              description={DUMMY_PAYMENT.description}
-              name={DUMMY_PAYMENT.name}
-              price={DUMMY_PAYMENT.price}
-              storeName={DUMMY_PAYMENT.storeName}
-              checked={true}
-              disable={true}
-              key={subs}
-            />
-          ))}
-      </div>
-      <PaymentInfo price={DUMMY_PAYMENT.price} />
-      <PaymentMethod
-        selectMethod={selectMethod}
-        setSelectMethod={setSelectMethod}
-        cashReceipts={cashReceipts}
-        setCashReceipts={setCashReceipts}
-        selectedCard={selectedCard}
-        setSelectedCard={setSelectedCard}
-        selectedBank={selectedBank}
-        setSelectedBank={setSelectedBank}
-      />
-      <Consent
-        isConsent={isConsent}
-        setIsConsent={setIsConsent}
-        storeName={DUMMY_PAYMENT.storeName}
-        consentArr={[
-          { content: "구독취소 등 환불 안내 확인 및 동의 (필수)", termsType: "refund" },
-          { content: "개인정보 수집 및 이용 동의 (필수)", termsType: "privacy" },
-          { content: "개인정보 제3자 제공 동의 (필수)", termsType: "privacyThirdParties" },
-          { content: "결제대행 서비스 이용 약관 동의 (필수) 토스페이먼츠", termsType: "payment" },
-        ]}
-      />
-      <div css={buttonWrapper}>
-        <button
-          css={buyButton(buttonActive())}
-          disabled={!buttonActive()}
-          onClick={() =>
-            push(
+      {!data ? (
+        <Spinner />
+      ) : (
+        <>
+          <div css={couponWrapper}>
+            {query.selectedSubs &&
+              (JSON.parse(query.selectedSubs as string) as string[]).map((subs) => (
+                <StoreCoupon
+                  type={data.type}
+                  id={data.subscribeId}
+                  count={data.useCount}
+                  description={data.intro}
+                  name={data.name}
+                  price={data.price}
+                  storeName={data.storeName}
+                  checked={true}
+                  disable={true}
+                  key={subs}
+                />
+              ))}
+          </div>
+          <PaymentInfo price={data?.price} />
+          <PaymentMethod
+            selectMethod={selectMethod}
+            setSelectMethod={setSelectMethod}
+            cashReceipts={cashReceipts}
+            setCashReceipts={setCashReceipts}
+            selectedBank={selectedBank}
+            setSelectedBank={setSelectedBank}
+          />
+          <Consent
+            isConsent={isConsent}
+            setIsConsent={setIsConsent}
+            storeName={data.storeName}
+            consentArr={[
+              { content: "구독취소 등 환불 안내 확인 및 동의 (필수)", termsType: "refund" },
+              { content: "개인정보 수집 및 이용 동의 (필수)", termsType: "privacy" },
+              { content: "개인정보 제3자 제공 동의 (필수)", termsType: "privacyThirdParties" },
               {
-                pathname: `${asPath}/complete`,
-                query: { price: DUMMY_PAYMENT.price },
+                content: "결제대행 서비스 이용 약관 동의 (필수) 토스페이먼츠",
+                termsType: "payment",
               },
-              `${asPath}/complete`
-            )
-          }
-        >
-          {DUMMY_PAYMENT.price.toLocaleString("ko-KR")}원 결제하기
-        </button>
-      </div>
+            ]}
+          />
+          <div css={buttonWrapper}>
+            <button
+              css={buyButton(buttonActive())}
+              disabled={!buttonActive()}
+              onClick={handlePayment}
+            >
+              {data.price.toLocaleString("ko-KR")}원 결제하기
+            </button>
+          </div>
+        </>
+      )}
     </Layout>
   );
 };
