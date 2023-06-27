ae0e1362f610722c0446e6cae628c29a4d4eade5
mynameisjisoo <devjisoolee@gmail.com>
2023-06-27 17:11:49 +0900

    공백제거, 코드 컨벤션 수정

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 import Layout from "common/layout";
 import { Colors, Sizes, Texts } from "styles/common";
 
-const wrapper = () => css`
+const wrapper = css`
   display: flex;
   flex-direction: column;
   text-align: center;
@@ -30,7 +30,7 @@ const wrapper = () => css`
 const ErrorPage = () => {
   const { push } = useRouter();
   return (
-    <Layout title="Not Found" subTitle="Not Found ">
+    <Layout title="Not Found" subTitle="Not Found">
       <div css={wrapper}>
         <h1>찾으시는 페이지가 없어요!</h1>
         <button onClick={() => push("/")}>메인페이지로 가기</button>
