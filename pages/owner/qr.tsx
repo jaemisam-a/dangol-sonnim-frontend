f5a56c43737f4dea6b6f251ee9c6483ef3eb2050
mynameisjisoo <devjisoolee@gmail.com>
2023-07-17 21:45:56 +0900

    단골 손님 QR인지 확인 코드 추가

--- pages/owner/qr.tsx
+++ pages/owner/qr.tsx
@@ -16,18 +16,25 @@ const QR = () => {
     <OwnerLayout title="QR코드" isLogo={true}>
       <QrReader
         showViewFinder={true}
-        delay={1000}
+        delay={500}
         onScan={(data) => {
           if (data) {
+            const isDangolQR = data?.substring(data.length - 7) === "-dangol";
+            const id = data.replace("-dangol", "");
+            if (!isDangolQR) return alert("단골손님 QR이 아닙니다.");
             if (confirm("방문 확인을 하시겠습니까?")) {
-              return mutateAsync(data)
+              return mutateAsync(id)
                 .then((res) => {
-                  res.remainCount > 0
+                  res.remainingCount > 0
                     ? setMessage("방문이 확인되었습니다.", false, "info")
                     : setMessage("사용 완료된 구독권입니다.", false, "warning");
                 })
                 .catch((err) => {
-                  setMessage("사용 완료된 구독권입니다.", false, "warning");
+                  setMessage(
+                    "QR코드 인증에 실패했습니다.\n다시 한번 확인해주세요.",
+                    false,
+                    "warning",
+                  );
                 });
             }
           }
